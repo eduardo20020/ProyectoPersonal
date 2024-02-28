c3b68387a62cef33283576292017e86c06840a1d
Brandon <brandonde2002@gmail.com>
2024-02-28 17:28:09 -0600

    normalidad

--- index.php
+++ index.php
@@ -5,53 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NeoGarcia</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2030498521976382" crossorigin="anonymous"></script>
-    <script async custom-element="amp-auto-ads"src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js"></script>
-    <script async custom-element="amp-ad" src="https://cdn.ampproject.org/v0/amp-ad-0.1.js"></script>
 </head>
 <body>
-<amp-ad width="100vw" height="320"
-     type="adsense"
-     data-ad-client="ca-pub-2030498521976382"
-     data-ad-slot="7708888412"
-     data-auto-format="mcrspv"
-     data-full-width="">
-  <div overflow=""></div>
-</amp-ad>
 
-    <amp-auto-ads type="adsense"
-            data-ad-client="ca-pub-2030498521976382">
-    </amp-auto-ads>
-
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2030498521976382"crossorigin="anonymous"></script>
-    <ins class="adsbygoogle"
-     style="display:block"
-     data-ad-format="autorelaxed"
-     data-ad-client="ca-pub-2030498521976382"
-     data-ad-slot="7708888412">
-    </ins>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
     <?php require "PUBLIC/navBar.php"; ?>
     <div class="container">
         <?php require "PUBLIC/carruzel.php"; ?>
     </div>
-
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2030498521976382" crossorigin="anonymous"></script>
-    <ins class="adsbygoogle"
-        style="display:block"
-        data-ad-format="autorelaxed"
-        data-ad-client="ca-pub-2030498521976382"
-        data-ad-slot="4749938186">
-    </ins>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
-    
-
-
-
 
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
